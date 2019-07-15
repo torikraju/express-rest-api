2c664a82a572faab210cbcb15ecdbf3047ca0f58
torikraju <torikraju@gmail.com>
2019-07-15 19:24:50 +0600

    adding db url with key

---
+++ backend/util/string.js
@@ -0,0 +1,4 @@
+exports.MONGODB_URI = 'mongodb+srv://torikraju:fKZPEn8Nuak4JFd@cluster0-shyre.mongodb.net/express_rest_api?retryWrites=true&w=majority';
+
+exports.jwtSecretKey = '8dCPsFjZcj';
+exports.jwtTokenExpiresIn = '1h';
